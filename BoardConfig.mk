e1c488c9c19f97563d869f3b1f37c32550adfcc6
Jingyanghao <129863338+Jingyanghao@users.noreply.github.com>
2024-08-25 02:55:19 +0800

    update

---
+++ BoardConfig.mk
@@ -0,0 +1,135 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/vivo/k6833v1_64_k419
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_BOARO_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := k6833v1_64_k419
+
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+
+# File system
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE :=ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE :=ext4
+BOARD_VENDORIMIMAGE_FILE_SYSTEM_TYPE :=ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=tty0 console=ttyS1,921600n1 root=/dev/ram 		vmalloc=400M slub_debug=OFZPU swiotlb=noforce 		firmware_class.path=/vendor/firmware 		cgroup.memory=nosocket,nokmem 		androidboot.hardware=mt6833 		page_owner=on loop.max_part=7 has_battery_removed=0 loop.max_part=7 androidboot.boot_devices=bootdevice,soc/11230000.mmc,11230000.mmc,soc/11270000.ufshci,11270000.ufshci ramoops.mem_address=0x48090000 ramoops.mem_size=0xe0000 ramoops.pmsg_size=0x10000 ramoops.console_size=0x40000 clk_ignore_unused usbserial_number=PFOFQ44TV8LJIZAA bootopt=64S3,32N2,64N2 product.version=PD2230_A_13.0.18.0.W10 fingerprint.abbr=12/SP1A.210812.003 region_ver=W10 product.solution=MTK buildvariant=user root=/dev/ram androidboot.vbmeta.device=PARTUUID=4ae2050b-5db5-4ff7-aad3-5730534be63d androidboot.vbmeta.avb_version=1.2 androidboot.vbmeta.device_state=unlocked androidboot.vbmeta.hash_alg=sha256 androidboot.vbmeta.size=12928 androidboot.vbmeta.digest=4dd80bebed3399fbea19eeb14a56cb324962395879b9f7a7af5ad5e38cf79479 androidboot.veritymode=enforcing androidboot.vc.magic=none androidboot.verifiedbootstate=orange androidboot.atm=disabled factory_mode=0 androidboot.vivo_brom_flag=1 androidboot.meta_log_disable=0 vivolog_flag=0 country_code=N product_version=PD2230C_A_13.0.18.0.W10.V000L1 oem_name=V000 factory_mode=0  tier=0 bbk_dp=0 androidboot.anti.sec_nonsec_reco=0,0,0 androidboot.anti.avb_anti_ver=0 mtk_printk_ctrl.disable_uart=1 androidboot.serialno=10CD360MX4001KL bbk_pcb_version=MP_0.1 androidboot.product.hardware.sku=11111101111 boot_storage=U_X androidboot.bootreason=kernel_panic gpt=1 usb2jtag_mode=0 androidboot.dtb_idx=0 androidboot.dtbo_idx=0
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x05400000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/vivo/k6833v1_64_k419
+TARGET_KERNEL_CONFIG := V2230A_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM := MT6833
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_CRYPTO := true
+
+# Logcat
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# Dynamic Partition
+BOARD_SUPER_PARTITION_SIZE := 12348030976
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_SIZE := 12348030976
+BOARD_MAIN_PARTITION_LIST := cust product system vendor vgc
+
+# System as root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_VBMETA_SYSTEM := system product
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
+BOARD_AVB_VBMETA_VENDOR := vendor
+BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 2
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 3
+
+# fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery.wipe
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_USES_RECOVERY_AS_ROOT := true
+TARGET_NO_RECOVERY := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := k6833v1_64_k419
+TARGET_NO_BOOTLOADER := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+
+# Display
+TARGET_SCREEN_DENSITY := 320
