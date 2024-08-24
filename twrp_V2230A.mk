9acb5be0dece007c20a9b59efc52f2165e951aa9
Jingyanghao <129863338+Jingyanghao@users.noreply.github.com>
2024-08-25 07:45:19 +0800

    更新 twrp_V2230A.mk

--- twrp_V2230A.mk
+++ twrp_V2230A.mk
@@ -19,6 +19,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Inherit from V2230A device
 $(call inherit-product, device/vivo/V2230A/device.mk)
 
