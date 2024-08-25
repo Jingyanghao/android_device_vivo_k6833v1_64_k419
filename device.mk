ffe151f40a62c124561549821e224593f3e6416f
Jingyanghao <129863338+Jingyanghao@users.noreply.github.com>
2024-08-25 12:16:03 +0800

    update

--- device.mk
+++ device.mk
@@ -1,11 +1,10 @@
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
+# Inherit from OEM SOC-common
+$(call inherit-product, $(COMMON_PATH)/common.mk)
 
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+LOCAL_PATH := device/vivo/PD2230
 
-LOCAL_PATH := device/vivo/V2230A
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS :=true
---
+++ twrp_V2230A.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit from TWRP product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device specific configs
+$(call inherit-product, device/vivo/PD2238/device.mk)
+
+PRODUCT_DEVICE := PD2230
+PRODUCT_NAME := twrp_PD2230
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := vivo
+PRODUCT_MANUFACTURER := V2230A
+PRODUCT_RELEASE_NAME := vivo
