2381fbbe543375b75fc38539cf025630bf643be3
Jingyanghao <129863338+Jingyanghao@users.noreply.github.com>
2024-08-25 13:10:42 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_V2230A-user
-add_lunch_combo omni_V2230A-userdebug
-add_lunch_combo omni_V2230A-eng
+add_lunch_combo omni_PD2230-user
+add_lunch_combo omni_PD2230-userdebug
+add_lunch_combo omni_PD2230-eng
